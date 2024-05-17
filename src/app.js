45f7606972a0f7c34a7e8d2f7fba32a0e22461c3
FbcGa <148731381+FbcGa@users.noreply.github.com>
2024-05-17 19:58:08 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let palo = Math.floor(Math.random() * 3);
+  let numCard = Math.floor(Math.random() * 12);
+  let typePalo = ["♦", "♥", "♠", "♣"];
+
+  let p = document.createElement("p");
+  p.innerHTML = `<span>${typePalo[palo]}</span> ${numCard} <span>${typePalo[palo]}</span>`;
+
+  if (palo === 0) {
+    p.classList.add("diamond");
+  } else if (palo === 1) {
+    p.classList.add("heart");
+  } else if (palo === 2) {
+    p.classList.add("spade");
+  } else {
+    p.classList.add("club");
+  }
+
+  let divContainer = document.getElementById("card-body");
+  divContainer.classList.add("naipe");
+
+  divContainer.appendChild(p);
 };
